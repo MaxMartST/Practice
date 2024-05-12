9097320539d007e519b703d0dbad9438f9815d30
MaxMartST <AdderGtx@gmail.com>
2024-05-12 12:10:50 +0300

    Реализовал вектор

---
+++ Lesson_6.1/Lesson_6.1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "MyVector.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+	MyVector<int> myVector1;
+
+	if (myVector1.Empty())
+	{
+		cout << "Вектор пуст" << endl;
+	};
+
+	cout << "Добавть значения в вектор" << endl;
+	myVector1.PushBack(1);
+	myVector1.PushBack(2);
+	myVector1.PushBack(3);
+	myVector1.PushBack(4);
+
+	if (!myVector1.Empty())
+	{
+		cout << "Вектор имеет размер: " + to_string(myVector1.Size()) << endl;
+		cout << "Вектор содержит: ";
+		for (int i = 0; i < myVector1.Size(); i++)
+		{
+			cout << myVector1[i] << " ";
+		};
+
+		cout << endl;
+	};
+
+	cout << "Подставим значение 0 в начало вектора" << endl;
+	myVector1.Insert(0, myVector1.Begin(), 0);
+
+	cout << "Вектор содержит: ";
+	for (int i = 0; i < myVector1.Size(); i++)
+	{
+		cout << myVector1[i] << " ";
+	};
+	cout << endl;
+
+	cout << "Удалить все значения с первой до последней позиции" << endl;
+	myVector1.Erase(myVector1.Begin(), myVector1.Size() - 1);
+
+	cout << "Вектор содержит: ";
+	for (int i = 0; i < myVector1.Size(); i++)
+	{
+		cout << myVector1[i] << " ";
+	};
+	cout << endl;
+
+	cout << "Очистить вектор" << endl;
+	myVector1.Clear();
+
+	cout << "Вектор имеет размер: " + to_string(myVector1.Size()) << endl;
+}
---
+++ Lesson_6.1/MyVector.h
@@ -0,0 +1,210 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+template<typename T> class MyVector
+{
+private:
+	T* arr;
+	int capacity, size;
+public:
+	MyVector()
+	{
+		arr = new T[1];
+		capacity = 1;
+		size = 0;
+	};
+	T& Front()
+	{
+		return arr[0];
+	};
+	T& Back()
+	{
+		return arr[size - 1];
+	};
+	T& End()
+	{
+		return arr[size];
+	};
+	auto Begin()
+	{
+		return arr[0];
+	};
+	bool Empty()
+	{
+		return (size == 0);
+	};
+	void ShrinkToFit()
+	{
+		capacity = size;
+		return;
+	};
+	void PushBack(T val)
+	{
+		if (size == capacity)
+		{
+			T* temp = new T[2 * capacity];
+			for (int i = 0; i < size; i++)
+			{
+				temp[i] = arr[i];
+			}
+			delete[] arr;
+			capacity *= 2;
+			arr = temp;
+		}
+		arr[size] = val;
+		size++;
+	};
+	void PopBack()
+	{
+		if (size == 0) {
+			cout << "Вектор пуст" << endl;
+			return;
+		}
+		size--;
+		T* temp = new T[size];
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = arr[i];
+		}
+		delete[] arr;
+		arr = temp;
+		ShrinkToFit();
+	};
+	void Clear()
+	{
+		while (Empty() == false)
+		{
+			PopBack();
+		};
+	};
+	void Insert(int n, int c = 1, T val = 0)
+	{
+		if (n == size) 
+		{
+			while (c > 0) 
+			{
+				PushBack(val);
+			};
+		}
+		else {
+			T* temp = new T[size + c];
+
+			int i = 0;
+			int ni = 0;
+			size = size + c;
+
+			for (ni; ni < size + 1; ni++) 
+			{
+				if (i != n) 
+				{
+					temp[ni] = arr[i];
+					i++;
+				}
+				else
+					if (i == n) 
+					{
+						if (c > 0) 
+						{
+							temp[ni] = val;
+							c--;
+						}
+						else 
+						{
+							temp[ni] = arr[i];
+							i++;
+						}
+					}
+					else 
+					{
+						cout << "ddd" << endl;
+					}
+			}
+			delete[] arr;
+			if (size > capacity)
+			{
+				capacity = size;
+			}
+			arr = temp;
+		}
+	};
+	void Erase(int pos1, int pos2 = -1)
+	{
+		if (pos2 == -1) 
+		{ 
+			pos2 = pos1; 
+		}
+		if (pos1 > pos2 || pos1 < 0 || pos2 < 0) 
+		{
+			cout << "Некорректно указаны позиции" << endl;
+			return;
+		};
+		if (pos2 > size - 1) {
+			cout << "Позиция выходит допустимый диапозон" << endl;
+			return;
+		}
+		if (Empty() == true) {
+			cout << "Вектор пуст" << endl;
+			return;
+		};
+
+		T* temp = new T[size];
+		int nsize = 0;
+		for (int i = 0; i < size; i++)
+		{
+			if (i < pos1 || i >= pos2) 
+			{
+				temp[nsize] = arr[i];
+				nsize++;
+			}
+		}
+
+		delete[] arr;
+		size = nsize;
+		arr = temp;
+	};
+	void Resize(int n, T val = 0)
+	{
+		if (n < size) 
+		{
+			while (n < size) 
+			{
+				PopBack();
+			}
+		}
+		else if (n > size) 
+		{
+			while (n > size) 
+			{
+				PushBack(val);
+			}
+		}
+	};
+	void Reserve()
+	{
+		if (size != 0) 
+		{
+			T* arr_r = new T[size];
+
+			for (int i = size; i > 0; i--)
+			{
+				arr_r[abs(i - size)] = arr[i - 1];
+			}
+			delete[] arr;
+			arr = arr_r;
+		}
+	};
+	auto Capacity()
+	{
+		return capacity;
+	};
+	auto Size()
+	{
+		return size;
+	};
+	T& operator[](int i)
+	{
+		return arr[i];
+	};
+};
+
